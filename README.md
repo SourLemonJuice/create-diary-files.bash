8de89ad24733c4c1ec899d9f9ccdef8e57b2257d
SourLemonJuice <SourLemonJuice233@outlook.com>
2024-06-23 17:09:21 +0800

    修改日期样式,改善代码风格
    - 时间变得更可读和准确了
    - /usr/bin/env bash
    - 报告错误或日志时添加 diary.bash: 字样

--- README.md
+++ README.md
@@ -1,15 +1,13 @@
-# 日记创建脚本(linux.bash)
+# diary.bash
 
-## 1.自述
-
-这是我写的第一个有实际作用的`bash`脚本哦
+这是我写的第一个有实际用途的`bash`脚本哦
 
 用来在一个文件夹里以固定的命名规则创建文本文件，也就是所谓的日记\
 我其实早就不用它了，毕竟写它有什么意义呢，都一样
 
 不管怎么说，一个一直陪着我学习 linux 和 bash 的git储存库对我来说还是很有价值的，所以做点修改加一个自述文件就上传上来啦
 
-## 2.配置
+## 配置变量
 
 作为配置项的变量都定义在脚本的顶部，真正需要改的应该是这些地方
 
@@ -22,20 +20,20 @@ FilePath="./"
 # 脚本会将文件路径接在字符串的最后面，比如 "vim --opt /file.txt"
 EditorExec="vim"
 #新文件的权限
-NewFilePermission="644"
+NewFilePermission="640"
 ```
 
 尤其是`FilePath`它是储存所有文件的目录，其结构类似这样
 
 ```text
 ${FilePath}
-├── date1.txt
-└── date2.txt
+├── 2024-01-01.txt
+└── 2024-01-02.txt
 ```
 
 `EditorExec`则为启动编辑器的命令，要编辑的文件路径会被加在命令的最后，也只能加在最后\
 `NewFilePermission`是创建文件的权限`chmod $NewFilePermission "$FileLocation"`
 
-## --.License
+## License
 
 This is free and unencumbered software released into the public domain.
--- diary.sh
+++ diary.sh
@@ -1,29 +1,29 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # This is free and unencumbered software released into the public domain.
 
-#获取日期
+# 获取日期
 Date=$(date +"%F")
-#获取时间
-Time=$(date +"%H:%M")
-#用日期当作文件名写进变量里
+# 获取时间
+Time=$(date +"%R(24h) %:z")
+# 用日期当作文件名写进变量里
 FileName="$Date.txt"
-#文件们应该都放在哪里
+# 文件们应该都放在哪里
 FilePath="./"
-#要使用的文本编辑器
+# 要使用的文本编辑器
 # 可以正常释放参数，比如 "vim --opt"
 # 不过请注意启动编辑器时使用的是 exec 命令，emm...不要搞出注入'攻击'就好，比如以 "-a xxx, -c, -l" 开头，详细参数可以执行 $(exec --help) 查看
 # 脚本会将文件路径接在字符串的最后面，比如 "vim --opt /file.txt"
 EditorExec="vim"
-#新文件的权限
-NewFilePermission="644"
+# 文件的权限（每次编辑时更改）
+NewFilePermission="640"
 
 # 输入选项
 Options=$(getopt -o hTtd -l help -- "$@")
 # 如果有错误参数导致getopt报错则退出
 if [ ! $? -eq 0 ];
 then
-	echo "获取参数出错"
-	exit 1
+    echo "diary.bash: 获取参数出错"
+    exit 1
 fi
 # 格式化getopt的输出
 eval set -- "$Options"
@@ -31,26 +31,26 @@ eval set -- "$Options"
 # 测试模式
 # if [ $1 == "-T" ];
 # then
-# 	#更改目录到脚本所在目录和名称
-# 	FilePath="../temp"
-# 	FileName="test."$FileName
+#     #更改目录到脚本所在目录和名称
+#     FilePath="../temp"
+#     FileName="test."$FileName
 
-# 	#输出变量
-# 	echo Date: $Date
-# 	echo Time: $Time
-# 	echo Name: $FileName
-# 	echo Path: $FilePath
-# 	echo EditorExec: $EditorExec
-# 	echo Filemode: $NewChangeMode
-# 	echo Option: $@
+#     #输出变量
+#     echo Date: $Date
+#     echo Time: $Time
+#     echo Name: $FileName
+#     echo Path: $FilePath
+#     echo EditorExec: $EditorExec
+#     echo Filemode: $NewChangeMode
+#     echo Option: $@
 
-# 	#让人能看见输出，留出反应的时间
-# 	read -p "按回车确认参数" -s
-# 	#read -p是不加换行的，自己打印一个空行
-# 	echo ""
+#     #让人能看见输出，留出反应的时间
+#     read -p "按回车确认参数" -s
+#     #read -p是不加换行的，自己打印一个空行
+#     echo ""
 
-# 	# 将变量移到下一位，不让后面的case重复检测
-# 	shift
+#     # 将变量移到下一位，不让后面的case重复检测
+#   shift
 # fi
 
 
@@ -61,41 +61,41 @@ FileLocation="$FilePath/$FileName"
 # 处理传入的参数，处理完后跳出
 while true;
 do
-	case $1 in
-		-t)
-			# 如果加入参数则插入 换行 时间
-			echo "">>"$FileLocation" || exit 1
-			echo "$Time">>"$FileLocation" || exit 1
-		;;
-		-d)
-			echo "删除操作"
-			# 最后确认
-			read -p "按回车键删除\"$FileLocation\"" -s
-			#read -p是不加换行的，自己打印一个空行
-			echo ""
-			rm "$FileLocation" || exit 1
-			exit 0
-		;;
-		-h | --help)
-			echo "这是一个快速创建日记的脚本"
-			echo "创建新文件时会默认加入当前日期与时间"
-			echo "[无效]-T 测试模式（仅第一位参数可用）"
-			echo "-t 插入当前时间"
-			echo "-d 删除当前文件"
-			echo "--help 查看有效的选项们"
-			exit 0
-		;;
-		--)
-			# 最后一个输出跳出
-			break
-		;;
-		?)
-			echo "无效参数，但已经在getopt定义"
-			exit 2
-		;;
-	esac
-	# 把 $1 向后移动变成原先 $2 的内容
-	shift
+    case $1 in
+        -t)
+            # 如果加入参数则插入 换行 时间
+            echo "">>"$FileLocation" || exit 1
+            echo "==== $Time ====">>"$FileLocation" || exit 1
+        ;;
+        -d)
+            echo "diary.bash: 正在执行删除操作"
+            # 最后确认
+            read -p "按回车键删除\"$FileLocation\"" -s
+            # read -p是不加换行的，自己打印一个空行
+            echo ""
+            rm -i "$FileLocation" || exit 1
+            exit 0
+        ;;
+        -h | --help)
+            echo "这是一个用于快速记录日记的脚本"
+            echo "创建新文件时会默认加入当前日期与时间"
+            echo "[已弃用] -T 测试模式（仅第一位参数可用）"
+            echo "-t 插入当前时间"
+            echo "-d 删除当前文件"
+            echo "--help 查看有效的选项们（本页面）"
+            exit 0
+        ;;
+        --)
+            # 最后一个输出跳出
+            break
+        ;;
+        ?)
+            echo "diary.bash: 无效参数，但已经在 getopt 中定义"
+            exit 2
+        ;;
+    esac
+    # 把 $1 向后移动变成原先 $2 的内容
+    shift
 done
 
 # 通用的逻辑
@@ -103,18 +103,18 @@ done
 # 创建文件
 if [ ! -f "$FileLocation" ];
 then
-	# 如果文件不存在
-	# 创建文件
-	touch "$FileLocation" || exit 1
-	if [ ! $? -eq 0 ];
-	then
-		echo "无法创建文件"
-		exit 3
-	fi
+    # 如果文件不存在
+    # 创建文件
+    touch "$FileLocation" || exit 1
+    if [ ! $? -eq 0 ];
+    then
+        echo "diary.bash: 无法创建文件"
+        exit 3
+    fi
 
-	# 设置权限
-	chmod $NewFilePermission "$FileLocation" || exit 1
-	echo "$Date $(date +%A) $Time">>"$FileLocation" || exit 1
+    # 设置权限
+    chmod $NewFilePermission "$FileLocation" || exit 1
+    echo "$Date $(date +%A) $Time">>"$FileLocation" || exit 1
 fi
 
 # 用编辑器打开文件
